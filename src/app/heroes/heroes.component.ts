81ebb076ab71982b100af8047a4e5c1f1fa85637
LytvynR <Ryslan.2240106@gmail.com>
2017-11-25 04:00:17 +0300

    services was added in the application

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -16,14 +17,18 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
